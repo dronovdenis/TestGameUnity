85904ec0995fe61219c342b1aa8e89da8fd06979
denis <denis.dronov.v@gmail.com>
2017-06-11 19:41:15 +0400

    Change project

---
+++ Project 2/Assets/Scripts/MoveBird.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MoveBird : MonoBehaviour {
+
+    public Transform bird;
+
+    private void OnMouseDrag() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        bird.position = new Vector2(mousePos.x, bird.position.y);
+        
+    }
+
+
+}
